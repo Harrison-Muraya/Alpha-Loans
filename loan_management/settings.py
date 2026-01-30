b08047d4f734782c51f9d4a9ac4f972af34b3034
Harrison-Muraya <harrisonmuraya8@gmail.com>
2026-01-31 00:34:41 +0300

    made allowed host into a list

--- loan_management/settings.py
+++ loan_management/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # DEBUG = True
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 
 # Application definition
