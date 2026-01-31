f86109bdcd7f9848753eed94ea46842265b1e2ce
Harrison-Muraya <harrisonmuraya8@gmail.com>
2026-01-31 14:10:53 +0300

    added static file route

--- loan_management/settings.py
+++ loan_management/settings.py
@@ -151,6 +151,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "theme" / "static",
+]
+
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
