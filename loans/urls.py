a3c6a1275879c0b6481c006f0634430b6b9bf065
Harrison-Muraya <harrisonmuraya8@gmail.com>
2026-03-22 17:20:35 +0300

    Added password reset flow to loans URLs

--- loans/urls.py
+++ loans/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -13,4 +14,28 @@ urlpatterns = [
     path('manange/borrower_summary/<int:user_id>/', views.borrower_detail, name='borrower_detail'),
     path('update/<int:loan_id>/', views.update_loan, name='update_loan'),
     path('delete/<int:loan_id>/', views.delete_loan, name='delete_loan'),
+
+    # Password reset flow
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='loans/password_reset.html',
+             email_template_name='loans/password_reset_email.txt',
+             subject_template_name='loans/password_reset_subject.txt',
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='loans/password_reset_done.html',
+         ),
+         name='password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='loans/password_reset_confirm.html',
+         ),
+         name='password_reset_confirm'),
+    path('password-reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='loans/password_reset_complete.html',
+         ),
+         name='password_reset_complete'),
 ]
